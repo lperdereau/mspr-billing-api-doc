c6bfd34c870710200d304a830befec37c990eed5
Jeremy Chauvin <jeremie.chauvin@epsi.fr>
2020-03-25 08:55:56 +0000

    Add doc v0.3.0

--- doc/v0.2.1/docs_config.js
+++ doc/v0.2.1/docs_config.js
@@ -6,4 +6,5 @@ var versionNodes = [
 {version: "v0.2.2", url: "https://billing.louisperdereau.fr/doc/v0.2.2/"},
 {version: "v0.2.3", url: "https://billing.louisperdereau.fr/doc/v0.2.3/"},
 {version: "v0.2.4", url: "https://billing.louisperdereau.fr/doc/v0.2.4/"},
+{version: "v0.3.0", url: "https://billing.louisperdereau.fr/doc/v0.3.0/"},
 ]
