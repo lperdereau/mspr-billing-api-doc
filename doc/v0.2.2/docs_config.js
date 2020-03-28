ef49e36e60af0a6084fcd2d9207a6b11dc223fd9
lperdereau <45263601+lperdereau@users.noreply.github.com>
2020-03-28 17:13:20 +0000

    Add doc v0.5.2

--- doc/v0.2.2/docs_config.js
+++ doc/v0.2.2/docs_config.js
@@ -13,4 +13,5 @@ var versionNodes = [
 {version: "v0.4.3", url: "https://billing.louisperdereau.fr/doc/v0.4.3/"},
 {version: "v0.5.0", url: "https://billing.louisperdereau.fr/doc/v0.5.0/"},
 {version: "v0.5.1", url: "https://billing.louisperdereau.fr/doc/v0.5.1/"},
+{version: "v0.5.2", url: "https://billing.louisperdereau.fr/doc/v0.5.2/"},
 ]
